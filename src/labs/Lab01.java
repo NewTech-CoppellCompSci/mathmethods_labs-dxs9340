45b388acd7ee195e0c7196d054717a98afa9404b
dxs9340 <dsharma@newtech.coppellisd.com>
2022-03-01 21:42:13 -0600

    -

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,5 +1,7 @@
 package labs;
 
+import java.util.Scanner;
+
 
 
 public class Lab01 {
@@ -17,6 +19,22 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner inkey = new Scanner(System.in);
+		
+		System.out.print("Enter a positive integer: ");
+		int input = inkey.nextInt();
+		System.out.println("");
+		
+		System.out.print("Enter another positive integer: ");
+		int input2 = inkey.nextInt();
+		System.out.println("");
+		
+		double result = Math.pow(input, input2);
+		int actresult = (int) result;
+		
+		System.out.println(input + "^" + input2 + " = " + result);
+		
+		System.out.println("\n\n\n ");
 		
 		
 	}
@@ -26,7 +44,16 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		Scanner inkey = new Scanner(System.in);
 		
+		System.out.println("Enter a positive integer: " );
+		double input = inkey.nextDouble();
+		System.out.println("");
+		
+		double result = Math.sqrt(input);
+		
+		System.out.println("The square root of " + input + " is " + result);
+		System.out.println("\n\n\n ");
 		
 	}
 
@@ -34,6 +61,24 @@ public class Lab01 {
 	
 
 	public static void problem03() {
+		
+		Scanner inkey = new Scanner(System.in);
+		
+		System.out.println("Lets create a triangle! \n"
+				+ "Start by entering some lengths . . .\n");
+		
+		System.out.print("Length A: ");
+		double lA = inkey.nextDouble();
+		System.out.print("Length B: ");
+		double lB = inkey.nextDouble();
+		
+		double lAsq = Math.pow(lA, 2);
+		double lBsq = Math.pow(lB, 2);
+		
+		double hypo = Math.sqrt(lAsq + lBsq);
+		
+		System.out.println("Hypotenuse = " + hypo);
+		System.out.println("\n\n\n ");
 		
 		
 		
@@ -45,7 +90,25 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
+		Scanner inkey = new Scanner(System.in);
+		double input = 0.1;
+		double min = 0;
+		double max = 0;
 		
+		while (input != 0) {
+			System.out.print("Enter an integer: ");
+			input = inkey.nextDouble();
+			
+			 if(input > max) {
+				 max = input;
+			 }
+			 else if (input < min ) {
+				 min = input;
+			 }
+			
+		}
+		System.out.println("Max = " + max);
+		System.out.println("Min = " + min);
 		
 	}
 	
@@ -54,4 +117,29 @@ public class Lab01 {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -17,7 +17,26 @@ public class Lab02 {
 	
 	public static void problem01() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter an "
+				+ "positive integer: ");
+		int input = inKey.nextInt();
 		
+		System.out.print("Enter another"
+				+ " positive integer: ");
+		int input2 = inKey.nextInt();
+		
+		int max = Math.max(input, input2);
+		int min = Math.min(input, input2);
+		int scope = max - min + 1;
+		
+		for(int i = 1; i <= 10; i++) {
+			
+			int num = (int) (Math.random() * scope + min);
+			System.out.print(num + " ");
+		}
+			System.out.println("\n\n");
 		
 		
 	}
@@ -27,14 +46,50 @@ public class Lab02 {
 	
 	public static void problem02() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter the radius: ");
+		double radius = inKey.nextInt();
 		
+		System.out.print("Enter the height: ");
+		double height = inKey.nextInt();
 		
+		double vol = (Math.PI) * 
+			(Math.pow(radius, 2)) * height;
+		
+		System.out.println("The cylinders "
+				+ "volume is " + vol);
+
 	}
 
 	
 	
 
 	public static void problem03() {
+		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter x value 1: ");
+		double x1 = inKey.nextInt();
+		
+		System.out.print("Enter y value 1: ");
+		double y1 = inKey.nextInt();
+		
+		System.out.print("Enter x value 2: ");
+		double x2 = inKey.nextInt();
+		
+		System.out.print("Enter y value 2: ");
+		double y2 = inKey.nextInt();
+		
+		double sect1 = (Math.pow((x2 - x1), 2));
+		double sect2 = (Math.pow((y2 - y1), 2));
+		
+		double dist = Math.sqrt(sect1 + sect2);
+		
+		System.out.println("The "
+				+ "distance between\n(" + x1 + "," +y1 + ")"
+						+ " and (" + x2 + "," + y2 + ")"
+								+ " is " + dist);
 
 		
 		
@@ -45,8 +100,28 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
+		Scanner inKey = new Scanner(System.in);
+
+		System.out.print("Enter a: ");
+		double a = inKey.nextInt();
 		
+		System.out.print("Enter b: ");
+		double b = inKey.nextInt();
 		
+		System.out.print("Enter c: ");
+		double c = inKey.nextInt();
+				
+		double sect1 = (b * -1)+ 
+				Math.sqrt(Math.pow(b, 2) - 4 * (a*c) );
+		double sect2 = (b * -1) - 
+				Math.sqrt(Math.pow(b, 2) - 4 * (a*c) );
+		double sect3 = 2 * a ;
+		
+		double x1 = sect1/sect3 ;
+		double x2 = sect2/sect3 ;
+		
+		System.out.println("x1 = " + x1);
+		System.out.print("x2 = " + x2);
 		
 	}
 	
